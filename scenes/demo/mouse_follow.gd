3e89d0025e095507666bc8ef4365e1f7f915cfc1
sirsegv <sirsegv@sirsegv.moe>
2026-08-23 19:40:55 +0930

    Add assets for jam

--- scenes/demo/mouse_follow.gd
+++
@@ -1,9 +0,0 @@
-extends AnimatableBody2D
-
-@export
-var speed = 100
-
-func _process(delta: float) -> void:
-	global_position = global_position.move_toward(get_viewport().get_mouse_position(), speed)
-	%Rope.set_point_position(0, global_position)
-	%Rope.set_point_position(1, %Balloon/RopeAnchor.global_position)
