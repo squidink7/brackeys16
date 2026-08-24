f238ab891e43abe5bb6d46bd664087fa4e2c7a30
H-mannDan <danielunirider@outlook.com>
2026-08-24 16:03:29 +0930

    Level/Lighting prototype

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,7 +1,15 @@
 extends Node
 
+#Redirect to lighting prototype scene
+''' 
 func start_level(level: int):
 	var world_scene = load('res://scenes/world/world.tscn').instantiate()
 	world_scene.set_level(level)
 	add_child(world_scene)
+	$MainMenu.hide()
+'''
+
+func start_level(level: int):
+	var lighting_scene = load('res://scenes/world/lighting_prototype.tscn').instantiate()
+	add_child(lighting_scene)
 	$MainMenu.hide()
