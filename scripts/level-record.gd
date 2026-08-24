e3762a734e04d579a2a9df597315991024d726ad
sirsegv <sirsegv@sirsegv.moe>
2026-08-24 16:36:21 +0930

    Update save system and add blur to lighting test

---
+++ scripts/level-record.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name LevelRecord
+
+var best_time: int
+var friends_encountered: int
+var total_attempts: int
+var deaths: int
--- scripts/save-data.gd
+++ scripts/save-data.gd
@@ -1,8 +1,14 @@
-class SaveData:
-	var level_records = {}
+extends Resource
+class_name SaveData
 
-class LevelRecord:
-	var besst_time: int
-	var friends_encountered: int
-	var total_attempts: int
-	var deaths: int
+var level_records = {}
+
+func set_level_record(level: int, record: LevelRecord):
+	if !(level in level_records):
+		level_records[level] = record
+		return
+	
+	level_records[level].best_time = min(level_records[record].best_time, record.best_time)
+	level_records[level].friends_encountered = max(level_records[record].friends_encountered, record.friends_encountered)
+	level_records[level].total_attempts += record.total_attempts
+	level_records[level].deaths += record.deaths
