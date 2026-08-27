42cd109018d827c8323c1be8ce3fed0037a14185
JoseCarlitosGordo <165242859+JoseCarlitosGordo@users.noreply.github.com>
2026-08-27 10:36:50 +0930

    Brightness of windows tied to energy value. Need to tie flashlight value to energy and test

--- scenes/entities/submarine/submarine.gd
+++ scenes/entities/submarine/submarine.gd
@@ -14,7 +14,10 @@ func _process(delta: float) -> void:
 	pass
 func take_damage(damage: int) -> void:
 	energy -= damage
+	var changed_color = modulate
+	modulate = Color.from_hsv(changed_color.h, changed_color.s, changed_color.v - 1, changed_color.a)
 func add_energy(amount: int) -> void:
 	energy += amount
+
 	
 	
