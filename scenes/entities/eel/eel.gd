6e9bf668f2bf161e8457f48d01857a646eb7d2b3
sirsegv <sirsegv@sirsegv.moe>
2026-08-27 10:56:54 +0930

    Submarine player

--- scenes/entities/eel/eel.gd
+++ scenes/entities/eel/eel.gd
@@ -11,5 +11,11 @@ func _physics_process(delta: float) -> void:
 		var angle_diff = angle_difference(rotation, direction.angle())
 		apply_torque(angle_diff * speed * 10)
 
-	$Sprites.scale.y = -1 if abs(rotation) > PI/2 else 1
+	if $Sprites.scale.y == -1:
+		if abs(rotation) < PI/2-0.1:
+			$Sprites.scale.y = 1
+	elif $Sprites.scale.y == 1:
+		if abs(rotation) > PI/2+0.1:
+			$Sprites.scale.y = -1
+
 	$Animation.speed_scale = linear_velocity.length() / 64
--- scenes/entities/submarine/submarine.gd
+++ scenes/entities/submarine/submarine.gd
@@ -1,23 +1,31 @@
 extends Entity
 class_name Submarine
 
+var frame: float = 0.0
+var frames_count: int = 0
 var energy: int
 @export var max_energy: int = 100
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	energy = max_energy # Replace with function body.
+	energy = max_energy
 
+	frames_count = $Sprites.get_child_count()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 func take_damage(damage: int) -> void:
 	energy -= damage
 	var changed_color = modulate
 	modulate = Color.from_hsv(changed_color.h, changed_color.s, changed_color.v - 1, changed_color.a)
+
 func add_energy(amount: int) -> void:
 	energy += amount
 
-	
-	
+func _physics_process(delta: float) -> void:
+	super(delta)
+
+	var current_speed = abs(linear_velocity.x) / 64
+	frame += current_speed
+	while frame > frames_count:
+		frame -= frames_count
+
+	for i in frames_count:
+		$Sprites.get_child(i).visible = i == int(frame)
