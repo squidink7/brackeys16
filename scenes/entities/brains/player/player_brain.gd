707f14c61c6ba0363502ab289d66aba411d77294
JoseCarlitosGordo <165242859+JoseCarlitosGordo@users.noreply.github.com>
2026-08-28 14:08:01 +0930

    Damage system and game over condition implemented

--- scenes/entities/brains/player/player_brain.gd
+++ scenes/entities/brains/player/player_brain.gd
@@ -8,8 +8,7 @@ func move(_entity: Entity, _delta: float) -> Vector2:
 	return Input.get_vector('move_left', 'move_right', 'move_up', 'move_down').normalized()
 
 func died() -> void:
-	if get_parent() is Entity:
-		
+	if get_parent() is Entity:	
 		if last_checkpoint:
 			print('Reviving at level ' + str(last_checkpoint.level))
 			get_parent().reset(last_checkpoint.global_position)
@@ -26,9 +25,10 @@ func set_light(on: bool) -> void:
 
 func set_checkpoint(cp: Checkpoint) -> void:
 	last_checkpoint = cp
+
 func end_game() -> void:
-	$GameOver.visible = !get_tree().paused
-	get_tree().paused = !get_tree().paused
+	$GameOver.visible = true
+	get_tree().paused = true
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("game_pause"):
