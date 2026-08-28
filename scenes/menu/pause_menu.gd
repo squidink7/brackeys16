46e15cbb1aca17368543bef47ab9cc26e238645a
Vanessa-Soetjiadi-01 <vanessasoetjiadi@gmail.com>
2026-08-28 16:48:04 +0930

    modified pause menu too, still haven't figured out light

--- scenes/menu/pause_menu.gd
+++ scenes/menu/pause_menu.gd
@@ -7,3 +7,6 @@ func resume() -> void:
 func exit() -> void:
 	get_tree().paused = false
 	$/root/Main.main_menu()
+
+func _process(delta): 
+	%PointLight2D.global_position = get_viewport().get_mouse_position()
